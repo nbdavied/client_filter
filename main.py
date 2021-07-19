1a4925fd473eb8277938ee2a231e8201ed2f1e61
Dai Wei <nbdavied@gmail.com>
2021-07-19 20:51:20 +0800

    读取客户对照表

--- main.py
+++ main.py
@@ -15,3 +15,27 @@ for row in sheet.rows:
     print(rowData)
 
 wb.close()
+
+# 读客户对照表
+wbClient = openpyxl.load_workbook('客户对照.xlsx')
+sheetClient = wbClient['客户']
+sheetStaff = wbClient['员工']
+# 存储客户对照数据
+clientStaffDict = {}
+rowIndex = 0
+for row in sheetClient.rows:
+    if rowIndex < 1:
+        rowIndex = rowIndex + 1
+        continue
+    clientName = row[0].value
+    staffName = row[1].value
+    clientStaffDict[clientName] = staffName
+print(clientStaffDict)
+# 存储员工数据
+staffs = []
+for row in sheetStaff.rows:
+    staffs.append(row[0].value)
+print(staffs)
+
+wbClient.save('客户对照.xlsx')
+wbClient.close()
