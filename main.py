fd7607c976bed6ab4058af41de6062008ab04433
Dai Wei <nbdavied@gmail.com>
2021-07-19 20:41:17 +0800

    读取原始数据表

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import openpyxl
 wb = openpyxl.load_workbook('原始数据.xlsx')
 sheet = wb['Sheet1']
 
+rowIndex = 0
+sourceData = []
+for row in sheet.rows:
+    if rowIndex < 1:
+        rowIndex = rowIndex + 1
+        continue
+    rowData = []
+    for cell in row:
+        rowData.append(cell.value)
+    print(rowData)
+
 wb.close()
